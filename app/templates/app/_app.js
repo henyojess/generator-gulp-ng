b9ba4ffd92467f4e3b65429336105d637a295862
henyojess <jessie@henyo.com>
2014-05-27 19:07:20 +0800

    fix for issue#3

--- app/templates/app/_app.js
+++ app/templates/app/_app.js
@@ -1,9 +1,12 @@
-'use strict';
+(function(){
+	'use strict';
 
-angular.module('<%= _.slugify(appname) %>', [ 'ngRoute','<%= _.slugify(appname) %>-main','templates' ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+	angular.module('<%= _.slugify(appname) %>', [ 'ngRoute','<%= _.slugify(appname) %>-main','templates' ])
+	  .config(function ($routeProvider) {
+	    $routeProvider
+	      .otherwise({
+	        redirectTo: '/'
+	      });
+	  });
+	  
+})();
--- app/templates/app/main/_main_controller.js
+++ app/templates/app/main/_main_controller.js
@@ -1,17 +1,21 @@
-'use strict';
+(function(){
+  'use strict';
 
-angular.module('<%= _.slugify(appname) %>-main',['ngRoute'])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'main/main.html',
-        controller: 'MainCtrl'
-      });
-  })
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+
+  angular.module('<%= _.slugify(appname) %>-main',['ngRoute'])
+    .config(function ($routeProvider) {
+      $routeProvider
+        .when('/', {
+          templateUrl: 'main/main.html',
+          controller: 'MainCtrl'
+        });
+    })
+    .controller('MainCtrl', function ($scope) {
+      $scope.awesomeThings = [
+        'HTML5 Boilerplate',
+        'AngularJS',
+        'Karma'
+      ];
+    });
+
+})();
